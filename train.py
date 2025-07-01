b9325f5d1f1dc3952b50439d600da30121ddc8b7
Тимур <khazratkulov8@gmail.com>
2025-07-01 21:05:13 +0300

    NN was improved

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
@@ -35,6 +35,8 @@ train_datagen = ImageDataGenerator(
     rotation_range=10,
     width_shift_range=0.1,
     height_shift_range=0.1,
+    zoom_range=0.1,
+    shear_range=0.1,
     horizontal_flip=True
 )
 val_datagen = ImageDataGenerator()
@@ -42,13 +44,23 @@ val_datagen = ImageDataGenerator()
 # Создание модели
 model = Sequential([
     Conv2D(32, (3,3), activation='relu', input_shape=(48,48,1)),
+    BatchNormalization(),
     MaxPooling2D((2,2)),
+    Dropout(0.25),
     Conv2D(64, (3,3), activation='relu'),
+    BatchNormalization(),
     MaxPooling2D((2,2)),
+    Dropout(0.25),
     Conv2D(128, (3,3), activation='relu'),
+    BatchNormalization(),
     MaxPooling2D((2,2)),
+    Dropout(0.25),
     Flatten(),
+    Dense(256, activation='relu'),
+    BatchNormalization(),
+    Dropout(0.5),
     Dense(128, activation='relu'),
+    BatchNormalization(),
     Dropout(0.5),
     Dense(7, activation='softmax')
 ])
